18ba1e030fbc890263eccd08f0195243c935a94f
dikobagda <dikobagda@gmail.com>
2026-04-20 21:19:27 +0700

    fixing gcs path

--- src/lib/gcs.ts
+++ src/lib/gcs.ts
@@ -1,7 +1,7 @@
 import { Storage } from '@google-cloud/storage';
 import path from 'path';
 
-const KEY_PATH = path.resolve(__dirname, '../../google-service-account.json');
+const KEY_PATH = path.join(process.cwd(), 'google-service-account.json');
 
 // Early check to help user
 import fs from 'fs';
